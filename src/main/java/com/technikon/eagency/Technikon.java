1e2908d351252c9454ec950c12567d087cd7b625
lonerae <78692050+lonerae@users.noreply.github.com>
2022-11-24 12:35:35 +0200

    added initialisations in main

--- src/main/java/com/technikon/eagency/Technikon.java
+++ src/main/java/com/technikon/eagency/Technikon.java
@@ -1,5 +1,16 @@
 package com.technikon.eagency;
 
+import com.technikon.eagency.repository.OwnerRepository;
+import com.technikon.eagency.repository.PropertyRepository;
+import com.technikon.eagency.repository.RepairRepository;
+import com.technikon.eagency.repository.impl.OwnerRepositoryImpl;
+import com.technikon.eagency.repository.impl.PropertyRepositoryImpl;
+import com.technikon.eagency.repository.impl.RepairRepositoryImpl;
+import com.technikon.eagency.services.AdministratorService;
+import com.technikon.eagency.services.OwnerService;
+import com.technikon.eagency.services.impl.AdministratorServiceImpl;
+import com.technikon.eagency.services.impl.OwnerServiceImpl;
+
 /**
  *
  * @author Nick
@@ -7,6 +18,13 @@ package com.technikon.eagency;
 public class Technikon {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        OwnerRepository ownerRepo = new OwnerRepositoryImpl();
+        PropertyRepository propertyRepo = new PropertyRepositoryImpl();
+        RepairRepository repairRepo = new RepairRepositoryImpl();
+        
+        OwnerService ownerService = new OwnerServiceImpl(ownerRepo, propertyRepo, repairRepo);
+        AdministratorService adminService = new AdministratorServiceImpl(repairRepo);
+        
     }
 }
