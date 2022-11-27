a27d68a3b135b047591e4993e56cbabe7f3cd941
lonerae <78692050+lonerae@users.noreply.github.com>
2022-11-27 12:13:25 +0200

    FIX: log and exception messages of administrator service, JavaDocs

--- src/main/java/com/technikon/eagency/exceptions/OwnerExceptionCodes.java
+++ src/main/java/com/technikon/eagency/exceptions/OwnerExceptionCodes.java
@@ -1,12 +1,9 @@
 package com.technikon.eagency.exceptions;
 
-/**
- *
- * @author Public
- */
 public class OwnerExceptionCodes {
 
     public final static String OWNER_IS_NULL = "The owner is null";
     public final static String OWNER_ALREADY_EXISTS = "The owner already exists";
-    public final static String OWNER_INVALID_DATA = "The given data are not appropriate to find an owner";
+    public final static String OWNER_INVALID_DATA = "The given data are not appropriate to create or update an owner";
+
 }
--- src/main/java/com/technikon/eagency/exceptions/PropertyExceptionCodes.java
+++ src/main/java/com/technikon/eagency/exceptions/PropertyExceptionCodes.java
@@ -1,12 +1,9 @@
 package com.technikon.eagency.exceptions;
 
-/**
- *
- * @author Public
- */
 public class PropertyExceptionCodes {
 
     public final static String PROPERTY_IS_NULL = "The property is null";
     public final static String PROPERTY_MISSING_DATA = "Not all data are given to create a property";
-    public final static String PROPERTY_INVALID_DATA = "The given data are not appropriate to find a property";
+    public final static String PROPERTY_INVALID_DATA = "The given data are not appropriate to create or update a property";
+
 }
